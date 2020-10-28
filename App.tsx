18c777e40cd670c87227dda610a01d0470bda88b
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2020-10-28 01:29:44 -0300

    Create navigation tabs

--- App.tsx
+++ App.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
+
+import { AppLoading } from 'expo';
+import { StatusBar } from 'expo-status-bar'
+
 import { StyleSheet, Text, View } from 'react-native';
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import { NavigationContainer } from '@react-navigation/native';
+import Tabs from './Routes/Tabs';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <NavigationContainer>
+          <Tabs />
+        </NavigationContainer>
+        <StatusBar style="light" />
+      </>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
