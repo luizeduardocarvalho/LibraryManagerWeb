9eedb31cf96aaea226ba5452b61d7de8c7f47bb1
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-01-08 00:21:49 +0000

    Add class ErrorHandlerHelper and remove unecessary unit tests

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,10 +1,9 @@
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { throwError } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 import { catchError } from 'rxjs/operators';
-import { Teacher } from 'src/models/teacher';
 import { User } from 'src/models/user';
+import { ErrorHandlerHelper } from './error-handler';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -24,21 +23,7 @@ export class AuthService {
 
   login(user: User): Observable<User> {
     return this.http.post<User>(this.baseUrl + 'login', user, httpOptions).pipe(
-      catchError(this.handleError)
+      catchError(ErrorHandlerHelper.handleError)
     );
-  }
-
-  private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
-      console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
-    }
-    // Return an observable with a user-facing error message.
-    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
--- src/services/teacher.service.ts
+++ src/services/teacher.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Teacher } from 'src/models/teacher';
 import { HttpHeaders } from '@angular/common/http';
+import { ErrorHandlerHelper } from './error-handler';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -27,22 +28,8 @@ export class TeacherService {
 
   createTeacher(teacher: Teacher): Observable<Teacher> {
     return this.http.post<Teacher>(this.baseUrl + 'teachers', teacher, httpOptions).pipe(
-      catchError(this.handleError)
+      catchError(ErrorHandlerHelper.handleError)
     );
-  }
-
-  private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
-      console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
-    }
-    // Return an observable with a user-facing error message.
-    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
   
 }
