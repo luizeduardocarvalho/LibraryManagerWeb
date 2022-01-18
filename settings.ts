0095a4275bedf7cf92ffb082ff59c0096b1bbe35
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-01-18 21:38:55 +0000

    adjust responsiveness

--- settings.ts
+++ settings.ts
@@ -1,1 +1,2 @@
-export const baseUrl = 'https://librarymanager-api.herokuapp.com/';
+// export const baseUrl = 'https://librarymanager-api.herokuapp.com/';
+export const baseUrl = 'https://localhost:5001/';
