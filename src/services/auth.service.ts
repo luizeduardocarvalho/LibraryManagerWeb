8a99b6f75c63fcdb3fd234c248b317ac252182ce
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-01-19 00:40:58 +0000

    change student list to css grid

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { catchError } from 'rxjs/operators';
+import { baseUrl } from 'settings';
 import { User } from 'src/models/user';
 import { ErrorHandlerHelper } from './error-handler';
 
@@ -17,12 +18,10 @@ const httpOptions = {
 })
 export class AuthService {
 
-  baseUrl = 'https://librarymanager-api.herokuapp.com/';
-
   constructor(private http: HttpClient) { }
 
   login(user: User): Observable<User> {
-    return this.http.post<User>(this.baseUrl + 'login/login', user, httpOptions).pipe(
+    return this.http.post<User>(baseUrl + 'login/login', user, httpOptions).pipe(
       catchError(ErrorHandlerHelper.handleError)
     );
   }
