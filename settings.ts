c27e4864f0606791885638f3dd52119316898418
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-01-20 12:59:51 +0000

    Fix css name pattern

--- settings.ts
+++ settings.ts
@@ -1,2 +1,2 @@
-export const baseUrl = 'https://librarymanager-api.herokuapp.com/';
-// export const baseUrl = 'https://localhost:5001/';
+// export const baseUrl = 'https://librarymanager-api.herokuapp.com/';
+export const baseUrl = 'https://localhost:5001/';
