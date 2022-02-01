85caab8272054a008f50b80a01aae541f4f26309
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-02-01 21:10:42 +0000

    Add toast to lend book and fix desktop css

--- src/app/book-list/lend-book/lend-book.component.ts
+++ src/app/book-list/lend-book/lend-book.component.ts
@@ -5,6 +5,7 @@ import { LendBook } from 'src/models/lend-book';
 import { Student } from 'src/models/student';
 import { BookService } from 'src/services/book.service';
 import { StudentService } from 'src/services/student.service';
+import { ToastService } from 'src/services/toast.service';
 
 @Component({
   selector: 'app-lend-book',
@@ -16,13 +17,15 @@ export class LendBookComponent implements OnInit {
   searchText: string = '';
   students: Student[] = [];
   bookId: number = 0;
+  error: boolean = false;
 
   constructor(
-    private studentService: StudentService, 
-    private route: ActivatedRoute, 
+    private studentService: StudentService,
+    private route: ActivatedRoute,
     private router: Router,
     private bookService: BookService,
-    private location: LocationStrategy) { }
+    private location: LocationStrategy,
+    private toastService: ToastService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
@@ -36,10 +39,30 @@ export class LendBookComponent implements OnInit {
     });
   }
 
+
   onLend(studentId: number) {
     let lendBook = new LendBook(studentId, this.bookId);
-    this.bookService.lendBook(lendBook).subscribe();
+    this.bookService.lendBook(lendBook).subscribe(
+      err => console.log(err),
+      (res: any) => {
+        if (res.status == 500 || res.status == 400) {
+          this.error = true;
+        }
+
+        if (this.error) {
+          this.redirect('Error', 'An error has occurred.', this.error);
+        }
+        else {
+          this.redirect('Success!', 'The book has been lent', this.error);
+        }
+      });
     this.router.navigate(['books', this.bookId]);
+  }
+
+  redirect(header: string, text: string, error: boolean) {
+    this.router.navigate(['/books']).then(() => {
+      this.toastService.show(text, header, error);
+    });
   }
 
   onBack() {
