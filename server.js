3258e0bc7b0dfeca2f9dc1443627d62c62e70d4e
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-02-25 21:10:05 +0000

    Remove cors usage

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ const express = require('express');
 const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
-var cors = require('cors');
 
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
-
-app.use(cors());
 
 app.get('/*', (req, res) => {
 res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
