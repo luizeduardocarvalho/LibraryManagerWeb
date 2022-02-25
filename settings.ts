f1da76cfe7c7a94983265b7d19d754bab3984ee1
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-02-25 02:12:12 +0000

    Add staging url

--- settings.ts
+++ settings.ts
@@ -1,2 +1,9 @@
-export const baseUrl = 'https://librarymanager-api.herokuapp.com/';
-// export const baseUrl = 'https://localhost:5001/';
+var baseUrl = 'https://localhost:5001/';
+
+if(window.location.origin.includes('staging')) {
+    baseUrl = 'https://librarymanager-api-staging.herokuapp.com/';
+} else {
+    baseUrl = 'https://librarymanager-api.herokuapp.com/'
+}
+
+export { baseUrl };
