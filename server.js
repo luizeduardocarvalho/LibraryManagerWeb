96b998d371838d6141a255307d595d49d4aa2049
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-02-25 17:50:06 +0000

    Add cors

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
+app.use(cors());
+
 app.get('/*', (req, res) => {
 res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
