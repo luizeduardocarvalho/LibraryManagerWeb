e6a7c5182c2fd423cc82700116472faf708f3b43
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-03-01 12:40:46 +0000

    Add teacher reference

--- src/models/student-transactions.ts
+++ src/models/student-transactions.ts
@@ -3,5 +3,6 @@ import { ITransaction } from "./ITransaction";
 export interface StudentWithTransactions {
     studentId: number;
     studentName: string;
+    teacherName: string;
     transactions: ITransaction[];
 }
--- src/models/teacher.ts
+++ src/models/teacher.ts
@@ -1,5 +1,6 @@
 export interface Teacher {
     id: number;
+    reference: number;
     name: string;
     email: string;
   }
