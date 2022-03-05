e4fa14e44c64b2f8f78ddda2ea5321f22f4af2df
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-03-05 16:32:08 +0000

    Add token for auth requests and remove default password

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,30 +5,32 @@ import { baseUrl } from 'settings';
 import { ChangePassword } from 'src/models/change-password';
 import { CreateUser } from 'src/models/create-user';
 import { User } from 'src/models/user';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type':  'application/json',
-    'Access-Control-Allow-Origin': '*'
-  })
-};
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Access-Control-Allow-Origin': '*',
+      Authorization: `${this.tokenService.getToken()}`
+    })
+  };
+
+  constructor(private http: HttpClient, private tokenService: TokenService) { }
 
   login(user: User): Observable<User> {
-    return this.http.post<User>(baseUrl + 'login/login', user, httpOptions);
+    return this.http.post<User>(baseUrl + 'login/login', user, this.httpOptions);
   }
 
   register(user: CreateUser) {
-    return this.http.post<CreateUser>(baseUrl + 'login/register', user);
+    return this.http.post<CreateUser>(baseUrl + 'login/register', user, this.httpOptions);
   }
 
   changePassword(user: ChangePassword) {
-    return this.http.patch<ChangePassword>(baseUrl + 'login/changepassword', user);
+    return this.http.patch<ChangePassword>(baseUrl + 'login/changepassword', user, this.httpOptions);
   }
 }
