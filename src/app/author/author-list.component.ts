f5b2c9e450b5cfe98fb9f6c80966c6be87df97cc
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-08-26 18:18:21 +0100

    Update author list page style

--- src/app/author/author-list.component.ts
+++ src/app/author/author-list.component.ts
@@ -1,25 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { GetAuthor } from 'src/models/get-author';
 import { AuthorService } from 'src/services/author.service';
 
 @Component({
-  selector: 'app-author-list',
   templateUrl: './author-list.component.html',
-  styleUrls: ['./author-list.component.scss']
+  styleUrls: ['./author-list.component.scss'],
 })
 export class AuthorListComponent implements OnInit {
-
   authors: GetAuthor[] = [];
   searchText: string = '';
+  isLoading = false;
 
-  constructor(private authorService: AuthorService) { }
+  constructor(
+    private authorService: AuthorService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void { }
-
-  search(searchText: any) {
-    this.authorService.getAuthorsByName(searchText).subscribe((authors: GetAuthor[]) => {
-      this.authors = authors;
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.searchText = params['name'];
     });
+
+    this.search(this.searchText);
   }
 
+  search(searchText: any) {
+    this.isLoading = true;
+    this.authorService
+      .getAuthorsByName(searchText)
+      .subscribe((authors: GetAuthor[]) => {
+        this.authors = authors;
+        this.isLoading = false;
+      });
+  }
 }
