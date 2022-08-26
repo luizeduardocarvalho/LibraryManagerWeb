f708a86ca2fa6204d9a0bc61e2b507c0f81484da
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-08-26 18:16:02 +0100

    Update book details page style

--- src/app/book/book-info/book-info.component.ts
+++ src/app/book/book-info/book-info.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BookDetails } from 'src/models/book-details';
 import { GetBook } from 'src/models/get-book';
 import { Transaction } from 'src/models/transaction';
@@ -8,17 +8,20 @@ import { BookService } from 'src/services/book.service';
 import { ToastService } from 'src/services/toast.service';
 
 @Component({
-  selector: 'app-book-info',
   templateUrl: './book-info.component.html',
   styleUrls: ['./book-info.component.scss'],
 })
 export class BookInfoComponent implements OnInit {
   bookId: number = 0;
   book?: BookDetails;
+  isLoading = false;
+  isRenewLoading = false;
+  isReturnLoading = false;
 
   constructor(
     private bookService: BookService,
     private route: ActivatedRoute,
+    private router: Router,
     private location: Location,
     private toastService: ToastService
   ) {}
@@ -36,6 +39,7 @@ export class BookInfoComponent implements OnInit {
   }
 
   getBooks() {
+    this.isLoading = true;
     this.bookService.getBookById(this.bookId).subscribe((book: BookDetails) => {
       this.book = new BookDetails(
         book.bookId,
@@ -43,23 +47,38 @@ export class BookInfoComponent implements OnInit {
         book.description,
         book.transactions
       );
+
+      if (book.description != null) {
+        this.book.description = this.book.description
+          .substring(0, 103)
+          .trimEnd()
+          .concat('...');
+      }
+
+      this.isLoading = false;
     });
   }
 
   onReturn() {
+    this.isReturnLoading = true;
     let getBook = new GetBook(this.bookId);
     this.bookService.returnBook(getBook).subscribe(
       (updatedTransaction: Transaction) => {
         if (updatedTransaction.transactionId != 0) {
           this.book?.setTransaction(updatedTransaction);
           this.toastService.show('Book returned!', 'Success!', false);
+          this.isReturnLoading = false;
         }
       },
-      (err: any) => this.toastService.show(err.error, 'Error', true)
+      (err: any) => {
+        this.toastService.show(err.error, 'Error', true);
+        this.isReturnLoading = false;
+      }
     );
   }
 
   onRenew() {
+    this.isRenewLoading = true;
     let getBook = new GetBook(this.bookId);
     this.bookService.renewBook(getBook).subscribe(
       (updatedTransaction: Transaction) => {
@@ -67,12 +86,24 @@ export class BookInfoComponent implements OnInit {
           this.book?.setTransaction(updatedTransaction);
           this.toastService.show('Book renewed!', 'Success!', false);
         }
+
+        this.isRenewLoading = false;
       },
-      (err: any) => this.toastService.show(err.error, 'Error', true)
+      (err: any) => {
+        this.toastService.show(err.error, 'Error', true);
+        this.isRenewLoading = false;
+      }
     );
   }
 
   onBack() {
-    this.location.back();
+    let state: any = this.location.getState();
+    if (state.search != undefined) {
+      this.router.navigate(['/books'], {
+        queryParams: { title: state.search },
+      });
+    } else {
+      this.location.back();
+    }
   }
 }
