d9507334ccdd175fb0341ba2a1bf451369942f47
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-08-26 18:13:08 +0100

    Change the search pipe name and normalize strings

--- src/pipes/search.pipe.ts
+++ src/pipes/search.pipe.ts
@@ -1,20 +1,27 @@
-import { Pipe, PipeTransform } from "@angular/core";
+import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({name: 'appFilter'})
+@Pipe({ name: 'search' })
 export class FilterPipe implements PipeTransform {
-    transform(items: any[], searchText: string): any[] {
-        if(!items) {
-            return [];
-        }
+  transform(items: any[], searchText: string): any[] {
+    if (!items) {
+      return [];
+    }
 
-        if(!searchText) {
-            return items;
-        }
+    if (!searchText) {
+      return items;
+    }
 
-        searchText = searchText.toLowerCase();
+    searchText = searchText.toLowerCase();
 
-        return items.filter(item => {
-            return item.name.toLowerCase().includes(searchText);
-        })
-    };
+    return items.filter((item) => {
+      let normalizedName = item.name
+        .toLowerCase()
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '');
+      let normalizedSearchText = searchText
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '');
+      return normalizedName.includes(normalizedSearchText);
+    });
+  }
 }
