5d3ed63687b7d5282212d5c51fa6822ae7a1bb64
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-08-26 18:13:23 +0100

    Add GetBokDetailsById endpoint

--- src/services/book.service.ts
+++ src/services/book.service.ts
@@ -15,50 +15,76 @@ import { catchError, map } from 'rxjs/operators';
 import { TokenService } from './token.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookService {
-
   httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type':  'application/json',
-      Authorization: `${this.tokenService.getToken()}`
-    })
+      'Content-Type': 'application/json',
+      Authorization: `${this.tokenService.getToken()}`,
+    }),
   };
 
-  constructor(private http: HttpClient, private tokenService: TokenService) { }
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
   getBooksByTitle(title: string): Observable<Book[]> {
     return this.http.get<Book[]>(baseUrl + 'books', {
-      params: { 'title': title },
-      headers: this.httpOptions.headers
+      params: { title: title },
+      headers: this.httpOptions.headers,
     });
   }
 
   getBookById(bookId: number): Observable<BookDetails> {
     return this.http.get<BookDetails>(baseUrl + 'books/getbookbyid', {
-      params: { 'bookId': bookId },
-      headers: this.httpOptions.headers
-    })
+      params: { bookId: bookId },
+      headers: this.httpOptions.headers,
+    });
+  }
+
+  getBookDetailsById(bookId: number): Observable<Book> {
+    return this.http.get<Book>(baseUrl + 'books/getbookdetails', {
+      params: { id: bookId },
+      headers: this.httpOptions.headers,
+    });
   }
 
   returnBook(book: GetBook): Observable<Transaction> {
-    return this.http.post<Transaction>(baseUrl + 'books/return', book, this.httpOptions);
+    return this.http.post<Transaction>(
+      baseUrl + 'books/return',
+      book,
+      this.httpOptions
+    );
   }
 
   renewBook(getBook: GetBook): Observable<Transaction> {
-    return this.http.patch<Transaction>(baseUrl + 'books/renew', getBook, this.httpOptions);
+    return this.http.patch<Transaction>(
+      baseUrl + 'books/renew',
+      getBook,
+      this.httpOptions
+    );
   }
 
   lendBook(lendBook: LendBook) {
-    return this.http.post<LendBook>(baseUrl + 'books/lend', lendBook, this.httpOptions);
+    return this.http.post<LendBook>(
+      baseUrl + 'books/lend',
+      lendBook,
+      this.httpOptions
+    );
   }
 
   createBook(book: CreateBook) {
-    return this.http.post<CreateBook>(baseUrl + 'books/create', book, this.httpOptions);
+    return this.http.post<CreateBook>(
+      baseUrl + 'books/create',
+      book,
+      this.httpOptions
+    );
   }
 
   updateBook(book: UpdateBook) {
-    return this.http.patch<CreateBook>(baseUrl + 'books/updatebook', book, this.httpOptions);
+    return this.http.patch<CreateBook>(
+      baseUrl + 'books/updatebook',
+      book,
+      this.httpOptions
+    );
   }
 }
