a9fd6752ac10a62b829f518005c44bfa1122af6b
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-08-27 19:12:05 +0100

    Adjust student list page style

--- src/app/student/student-list/student-list.component.ts
+++ src/app/student/student-list/student-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Student } from 'src/models/student';
 import { StudentService } from 'src/services/student.service';
 
@@ -9,16 +10,28 @@ import { StudentService } from 'src/services/student.service';
 export class StudentListComponent implements OnInit {
   students: Student[] = [];
   searchText: string = '';
+  isLoading = false;
 
-  constructor(private studentService: StudentService) {}
+  constructor(
+    private studentService: StudentService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.searchText = params['name'];
+    });
+
+    this.search(this.searchText);
+  }
 
   search(searchText: any) {
+    this.isLoading = true;
     this.studentService
       .getStudentsByName(searchText)
       .subscribe((students: Student[]) => {
         this.students = students;
+        this.isLoading = false;
       });
   }
 }
